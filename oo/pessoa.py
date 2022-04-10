3f88c34cb38f19f5c757a30f71d30cd08d229a57
mateusschemer <mateus.schemer@gmail.com>
2022-04-10 19:19:32 -0300

    Criados atributos de instancia nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=38):
+    def __init__(self, *filhos, nome=None, idade=38):
         self.idade=idade
         self.nome = nome
-
+        self.filhos=list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Mateus')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    mateus = Pessoa(nome='Mateus')
+    helena = Pessoa(mateus, nome='Helena')
+    print(Pessoa.cumprimentar(mateus))
+    print(id(mateus))
+    print(mateus.cumprimentar())
+    print(mateus.nome)
+    print(mateus.idade)
+    print(helena.filhos)
 
